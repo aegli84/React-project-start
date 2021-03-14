298e37c6c59e9b0afcecefbc679165c4b9a969f3
aegli84 <egli.andreea@yahoo.com>
2021-03-14 23:25:36 +0100

    v1.4

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -2,14 +2,17 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
 
-     /* * {
+      /* * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-    } */
+    }  */
 
     body{
         background: #dee2e6;
+        width: 80%;
+        text-align: center;
+        margin: auto;
     }
 `;
 
